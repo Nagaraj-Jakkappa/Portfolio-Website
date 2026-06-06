148e0f631bb00123619a61237bbc124c17322401
nagupoojary33-sys <nagupoojary33@gmail.com>
2026-06-06 23:29:43 +0530

    feat: add learningOutcomes array to Certificate schema

--- server/models/Certificate.js
+++ server/models/Certificate.js
@@ -5,6 +5,7 @@ const certificateSchema = new mongoose.Schema({
   organization: { type: String, required: true },
   organizationLogo: { type: String }, // Field for image URL
   date: { type: String },
+  learningOutcomes: [{ type: String }], // New field for outcomes and takeaways
   link: { type: String },
   description: { type: String },
   createdAt: { type: Date, default: Date.now },
